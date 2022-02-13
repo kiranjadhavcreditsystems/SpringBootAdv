96dd5b7adef919f11277a488968498ea292bc7fb
kiranjadhavcreditsystems <contact@creditsystemsindia.com>
2022-02-13 11:25:44 +0530

    initial changes

---
+++ SpringBootAdv/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+    @GetMapping
+    public String sayWelcome()
+    {
+        return "WELCOME TO CSI PUNE";
+    }
+
+    @GetMapping("/address")
+    public String sayCSIAddress()
+    {
+        return "INSPIRIA MALL |  PUNE";
+    }
+}
